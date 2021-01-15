4619c79221003b4ba8e7a59c3a1f377d3e77d010
otaken46 <xotaken46@gmail.com>
2021-01-16 01:40:24 +0900

    動作確認

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class ForceHttps
 {
@@ -16,6 +17,7 @@ class ForceHttps
      */
     public function handle($request, Closure $next)
     {
+        Log::debug($request);
         if (App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
             return redirect()->secure($request->getRequestUri());
         }
