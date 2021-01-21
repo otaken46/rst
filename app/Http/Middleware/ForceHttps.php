60e7fdb187b8b61bca7479f43c23b00e636ddfc4
otaken46 <xotaken46@gmail.com>
2021-01-21 18:58:02 +0900

    動作確認

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -17,23 +17,9 @@ class ForceHttps
      */
     public function handle($request, Closure $next)
     {
-        Log::debug('message00');
-        if(array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER)){
-            if (App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
-                Log::debug('message01');
-                return redirect('/');
-            }
-            Log::debug('message02');
-            return $next($request);
-        }else{
-            if (App::environment(['local'])){
-                Log::debug('message03');
-                return $next($request);
-            }else{
-                Log::debug('message04');
-                return redirect('/');
-            }
-        }
-        
+//        if (App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+//            return redirect()->secure($request->getRequestUri());
+//        }
+        return $next($request);
     }
 }
