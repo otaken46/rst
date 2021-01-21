28260e20b4133cc4b17e402b1515b6e2b71807f9
otaken46 <xotaken46@gmail.com>
2021-01-21 18:25:37 +0900

    動作確認

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,7 +26,6 @@ class LoginController extends Controller
     }
     public function user_check (Request $request)
     {
-        
         $errors = '';
         $sql_result = 0;
         $sql = 0;
