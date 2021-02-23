b1a69b8a79a27e4c1e966708f2420a24f16cbb36
otaken46 <xotaken46@gmail.com>
2021-02-23 18:16:19 +0900

    ver1.4

--- resources/views/chart_patient.blade.php
+++ resources/views/chart_patient.blade.php
@@ -123,8 +123,7 @@
       ※ バックエンドとの連携後、削除してください
   -->
   <script type="text/javascript">
-  var chart_data = new Array()
-  chart_data = {{$chart_patient}};
+  var chart_data = @json($chart_patient);
   var old_date = '{{$old_date}}';
   var target_id ='{{$patient_id}}';
   var memoChoices =@json($memo_list);
