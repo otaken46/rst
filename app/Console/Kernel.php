1945aab02446d611ec349bae342afd0053d108dc
otaken46 <xotaken46@gmail.com>
2021-03-04 20:51:50 +0900

    動作確認

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -352,7 +352,7 @@ class Kernel extends ConsoleKernel
                 }
                 ini_set('memory_limit', '256M');
             }
-        })->dailyAt('06:00');
+        })->dailyAt('20:53');
     }
     public static function operation_log($userid, $operation_code, $result = NULL){
         $operation_log = new OperationLog();
